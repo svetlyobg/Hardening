aa388926e60cd44df2e0f7c803ba98105664c4aa
svetlyobg <swk_12@abv.bg>
2021-05-11 00:28:01 +0300

    # V-220707 - The Windows 10 system must use an anti-virus program

--- Microsoft/Windows 10/CAT I (High) 26/high26.ps1
+++ Microsoft/Windows 10/CAT I (High) 26/high26.ps1
@@ -4,3 +4,6 @@ Get-Volume | Where-Object -Property FilesystemType -ne NTFS
 
 # V-220706
 winver
+
+# V-220707 - The Windows 10 system must use an anti-virus program
+Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct
