dab50aed7edc65ccf38574f96304b571f40268b4
svetlyobg <swk_12@abv.bg>
2021-05-11 00:39:13 +0300

    # V-220932 - Anonymous access to Named Pipes and Shares must be restricted
    Added $RestrictNullSessAccess

--- Microsoft/Windows 10/CAT I (High) 26/high26.ps1
+++ Microsoft/Windows 10/CAT I (High) 26/high26.ps1
@@ -24,9 +24,20 @@ Fix Text (F-22636r555282_fix)
 Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Local Policies >> Security Options >> "Network access: Restrict anonymous access to Named Pipes and Shares" to "Enabled".
 #>
 
-$var = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters -Name RestrictNullSessAccess 
+$RestrictNullSessAccess = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters -Name RestrictNullSessAccess 
 
-if ( Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters -Name RestrictNullSessAccess -eq 1)
+
+
+if ( $RestrictNullSessAccess.RestrictNullSessAccess -eq 1)
 {
-    Write-Host "V-220932 is " $var
+    Write-Host V-220932 result is $RestrictNullSessAccess.RestrictNullSessAccess
+    Write-Host
+    Write-Host "and"
+    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters -Name RestrictNullSessAccess 
+}
+
+else
+{
+Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters -Name RestrictNullSessAccess 
+Write-Host V-220932 result is $RestrictNullSessAccess.RestrictNullSessAccess
 }
