e7870c55fc483270671a27cd20d4007c5c399439
svetlyobg <swk_12@abv.bg>
2021-05-11 00:30:25 +0300

    V-220932 - Anonymous access to Named Pipes and Shares must be restricted

--- Microsoft/Windows 10/CAT I (High) 26/high26.ps1
+++ Microsoft/Windows 10/CAT I (High) 26/high26.ps1
@@ -7,3 +7,26 @@ winver
 
 # V-220707 - The Windows 10 system must use an anti-virus program
 Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct
+
+# V-220932 - Anonymous access to Named Pipes and Shares must be restricted
+
+<# 
+If the following registry value does not exist or is not configured as specified, this is a finding:
+
+Registry Hive: HKEY_LOCAL_MACHINE
+Registry Path: \SYSTEM\CurrentControlSet\Services\LanManServer\Parameters\
+
+Value Name: RestrictNullSessAccess
+
+Value Type: REG_DWORD
+Value: 1
+Fix Text (F-22636r555282_fix)
+Configure the policy value for Computer Configuration >> Windows Settings >> Security Settings >> Local Policies >> Security Options >> "Network access: Restrict anonymous access to Named Pipes and Shares" to "Enabled".
+#>
+
+$var = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters -MemberType NoteProperty -Name RestrictNullSessAccess 
+
+if ( Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters -Name RestrictNullSessAccess -eq 1)
+{
+    Write-Host "V-220932 is " $var
+}
