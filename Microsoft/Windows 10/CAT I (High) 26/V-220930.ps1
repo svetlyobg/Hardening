31e952c56c66ad4a58b8f2bbd4c5d8257941dddf
svetlyobg <swk_12@abv.bg>
2021-05-11 10:49:32 +0300

    select -ExpandProperty restrictanonymous

--- Microsoft/Windows 10/CAT I (High) 26/V-220930.ps1
+++ Microsoft/Windows 10/CAT I (High) 26/V-220930.ps1
@@ -1,21 +1,17 @@
 ﻿# V-220930 - Anonymous enumeration of shares must be restricted.
 
-$RestrictAnonymous = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous
+$RestrictAnonymous = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous | select -ExpandProperty restrictanonymous
 
-if ( $RestrictAnonymous.RestrictNullSessAccess -eq 1)
+if ( $RestrictAnonymous -eq 1)
 {
-    Write-Host V-220930 result is $RestrictAnonymous.RestrictAnonymous
-    Write-Host
-    Write-Host "and"
-    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous 
+    Write-Host V-220930 result is $RestrictAnonymous
+    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous | select -ExpandProperty restrictanonymous
     Write-Host "This is not a finding" -ForegroundColor Green
 }
 
 else 
 {
-    Write-Host V-220930 result is $RestrictAnonymous.RestrictAnonymous
-    Write-Host
-    Write-Host "and"
-    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous 
+    Write-Host V-220930 result is $RestrictAnonymous
+    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous|  select -ExpandProperty restrictanonymous
     Write-Host "This is a finding" -ForegroundColor Red
 }
