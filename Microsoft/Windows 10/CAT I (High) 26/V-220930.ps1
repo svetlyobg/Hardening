922c59dcd29e2b4ea5c4a8c265cfe399aba127dd
svetlyobg <swk_12@abv.bg>
2021-05-11 10:43:09 +0300

    V-220930 - Anonymous enumeration of shares must be restricted.

---
+++ Microsoft/Windows 10/CAT I (High) 26/V-220930.ps1
@@ -0,0 +1,21 @@
+﻿# V-220930 - Anonymous enumeration of shares must be restricted.
+
+$RestrictAnonymous = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous
+
+if ( $RestrictAnonymous.RestrictNullSessAccess -eq 1)
+{
+    Write-Host V-220930 result is $RestrictAnonymous.RestrictAnonymous
+    Write-Host
+    Write-Host "and"
+    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous 
+    Write-Host "This is not a finding" -ForegroundColor Green
+}
+
+else 
+{
+    Write-Host V-220930 result is $RestrictAnonymous.RestrictAnonymous
+    Write-Host
+    Write-Host "and"
+    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous 
+    Write-Host "This is a finding" -ForegroundColor Red
+}
