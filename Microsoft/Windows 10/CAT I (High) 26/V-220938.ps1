7177671fcb116df535b5cd94f55d114e990af5ae
svetlyobg <swk_12@abv.bg>
2021-05-11 12:14:23 +0300

    V-220938 - The LanMan authentication level must be set to send NTLMv2 response only, and to refuse LM and NTLM

---
+++ Microsoft/Windows 10/CAT I (High) 26/V-220938.ps1
@@ -0,0 +1,23 @@
+﻿# V-220938 - The LanMan authentication level must be set to send NTLMv2 response only, and to refuse LM and NTLM
+# https://stigviewer.com/stig/windows_10/2020-10-15/finding/V-220938
+
+$LmCompatibilityLevel = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa | select -ExpandProperty LmCompatibilityLevel
+
+if ( $LmCompatibilityLevel -eq 5)
+{
+    Write-Host "`n$line"
+    Write-Host V-220932 result is $LmCompatibilityLevel
+    Write-Host "`n$line"
+    Write-Host "This is not a finding" -ForegroundColor Green
+    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name LmCompatibilityLevel
+    
+}
+
+else
+    {
+        Write-Host "`n$line"
+        Write-Host V-220932 result is $LmCompatibilityLevel
+        Write-Host "`n$line"
+        Write-Host "This is a finding" -ForegroundColor Red
+        Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name LmCompatibilityLevel
+    }
