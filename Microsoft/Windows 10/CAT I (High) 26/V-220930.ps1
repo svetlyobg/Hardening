1b0d86d5d6ece7ab27294605e5165373f96eae0d
svetlyobg <swk_12@abv.bg>
2021-05-11 11:28:23 +0300

    Update V-220930 - adding expanded property

--- Microsoft/Windows 10/CAT I (High) 26/V-220930.ps1
+++ Microsoft/Windows 10/CAT I (High) 26/V-220930.ps1
@@ -1,17 +1,24 @@
-﻿# V-220930 - Anonymous enumeration of shares must be restricted.
+﻿# V-220930 - Anonymous enumeration of shares must be restricted
 
 $RestrictAnonymous = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous | select -ExpandProperty restrictanonymous
 
 if ( $RestrictAnonymous -eq 1)
 {
+    Write-Host
     Write-Host V-220930 result is $RestrictAnonymous
-    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous | select -ExpandProperty restrictanonymous
+    Write-Host    
     Write-Host "This is not a finding" -ForegroundColor Green
+
+    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous
 }
 
 else 
 {
+    Write-Host
     Write-Host V-220930 result is $RestrictAnonymous
-    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous|  select -ExpandProperty restrictanonymous
+    Write-Host
     Write-Host "This is a finding" -ForegroundColor Red
+
+    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictAnonymous
+    
 }
