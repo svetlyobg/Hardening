be661598d21cf8ae3fb605c1ceeba70ecdcb67f0
svetlyobg <swk_12@abv.bg>
2021-05-11 21:41:11 +0300

    no-V-220737 - Administrative accounts must not be used with applications that access the Internet, such as web browsers, or with potential Internet sources, such as email

---
+++ Microsoft/Windows 10/CAT I (High) 26/no-V-220737.ps1
@@ -0,0 +1,4 @@
+﻿# V-220737 - Administrative accounts must not be used with applications that access the Internet, such as web browsers, or with potential Internet sources, such as email
+# https://www.stigviewer.com/stig/windows_10/2020-10-15/finding/V-220737
+
+# No Implementation
