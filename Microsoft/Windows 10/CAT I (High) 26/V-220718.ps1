1324439326327d05ed5edcda05ab430b3478f8f8
svetlyobg <swk_12@abv.bg>
2021-05-11 15:22:59 +0300

    V-220718 - Internet Information System (IIS) or its subcomponents must not be installed on a workstation

---
+++ Microsoft/Windows 10/CAT I (High) 26/V-220718.ps1
@@ -0,0 +1,23 @@
+﻿#  V-220718 - Internet Information System (IIS) or its subcomponents must not be installed on a workstation
+# https://stigviewer.com/stig/windows_10/2020-10-15/finding/V-220718
+
+$State = Get-WindowsOptionalFeature -FeatureName IIS-WebServerRole  -Online | select -ExpandProperty State
+
+    if ( $State -eq 'Disabled')
+{
+    Write-Host "`n$line"
+    Write-Host V-220718 result is $State 
+    Write-Host "`n$line"
+    Write-Host "This is not a finding" -ForegroundColor Green
+    Get-WindowsOptionalFeature -FeatureName IIS-WebServerRole  -Online 
+    
+}
+
+else
+    {
+        Write-Host "`n$line"
+        Write-Host V-220718 result is $State
+        Write-Host "`n$line"
+        Write-Host "This is a finding" -ForegroundColor Red
+        Get-WindowsOptionalFeature -FeatureName IIS-WebServerRole  -Online 
+    }
