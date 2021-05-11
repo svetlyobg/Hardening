f8d9aa26b9bab5eca1e033a5fa77c2701a1ef31c
svetlyobg <swk_12@abv.bg>
2021-05-11 20:50:06 +0300

    V-220726 - Data Execution Prevention (DEP) must be configured to at least OptOut

---
+++ Microsoft/Windows 10/CAT I (High) 26/V-220726.ps1
@@ -0,0 +1,38 @@
+﻿# V-220726 - Data Execution Prevention (DEP) must be configured to at least OptOut
+# https://www.stigviewer.com/stig/windows_10/2020-10-15/finding/V-220726
+
+function f
+    {
+         get-WmiObject Win32_OperatingSystem |  Select-Object DataExecutionPrevention_SupportPolicy -ExpandProperty DataExecutionPrevention_SupportPolicy 
+    }
+
+function f2
+    {
+         wmic OS Get DataExecutionPrevention_SupportPolicy
+    }
+
+$dep = f
+
+
+
+    if ( $dep -eq 1 -or $dep -eq 3)
+                            {
+                                Write-Host "`n$line"
+                                Write-Host V-220726 result is $dep 
+                                Write-Host "`n$line"
+                                Write-Host "This is not a finding" -ForegroundColor Green
+                                Write-Host "`n$line"
+                                f2
+    
+                            }
+
+    else
+                            {
+                                Write-Host "`n$line"
+                                Write-Host V-220726 result is $dep
+                                Write-Host "`n$line"
+                                Write-Host "This is a finding" -ForegroundColor Red
+                                Write-Host "`n$line"
+                                f2
+                            }
+
