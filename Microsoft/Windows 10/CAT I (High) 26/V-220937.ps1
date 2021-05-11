1994ca04a766611ca94c4d53c477e28447e0c1da
svetlyobg <swk_12@abv.bg>
2021-05-11 12:11:22 +0300

    V-220937 - The system must be configured to prevent the storage of the LAN Manager hash of passwords

---
+++ Microsoft/Windows 10/CAT I (High) 26/V-220937.ps1
@@ -0,0 +1,23 @@
+﻿# V-220937 - The system must be configured to prevent the storage of the LAN Manager hash of passwords
+# https://stigviewer.com/stig/windows_10/2020-10-15/finding/V-220937
+
+$NoLMHash = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa | select -ExpandProperty NoLMHash
+
+if ( $NoLMHash -eq 1)
+{
+    Write-Host "`n$line"
+    Write-Host V-220932 result is $NoLMHash
+    Write-Host "`n$line"
+    Write-Host "This is not a finding" -ForegroundColor Green
+    Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name NoLMHash
+    
+}
+
+else
+    {
+        Write-Host "`n$line"
+        Write-Host V-220932 result is $NoLMHash
+        Write-Host "`n$line"
+        Write-Host "This is a finding" -ForegroundColor Red
+        Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name NoLMHash
+    }
