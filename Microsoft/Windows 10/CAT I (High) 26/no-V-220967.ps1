4d5cc5621bd04aa3cce02ad68c21e4046bc8991c
svetlyobg <swk_12@abv.bg>
2021-05-18 20:37:45 +0300

    V-220967 - Administrative accounts must not be used with applications that access the Internet, such as web browsers, or with potential Internet sources, such as email

---
+++ Microsoft/Windows 10/CAT I (High) 26/no-V-220967.ps1
@@ -0,0 +1,4 @@
+﻿# V-220967 - Administrative accounts must not be used with applications that access the Internet, such as web browsers, or with potential Internet sources, such as email
+# https://www.stigviewer.com/stig/windows_10/2020-10-15/finding/V-220967
+
+# No Implementation
