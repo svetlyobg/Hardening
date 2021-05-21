ccb54fb9e5eafa893ed94f2063fa5ef6abac33e1
svetlyobg <swk_12@abv.bg>
2021-05-21 16:08:52 +0300

    V-220865 - The Windows Remote Management (WinRM) service must not use Basic authentication

---
+++ Microsoft/Windows 10/CAT I (High) 26/V-220865.ps1
@@ -0,0 +1,23 @@
+﻿# V-220865 - The Windows Remote Management (WinRM) service must not use Basic authentication
+# https://stigviewer.com/stig/windows_10/2020-10-15/finding/V-220865
+
+$AllowBasic = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service\' | Select -ExpandProperty AllowBasic
+
+    if ( $AllowBasic -eq 0)
+{
+    Write-Host "`n$line"
+    Write-Host V-220865 result is $AllowBasic 
+    Write-Host "`n$line"
+    Write-Host "This is not a finding" -ForegroundColor Green
+    Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client\'
+    
+}
+
+else
+    {
+        Write-Host "`n$line"
+        Write-Host V-220865 result is $AllowBasic
+        Write-Host "`n$line"
+        Write-Host "This is a finding" -ForegroundColor Red
+        Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client\' 
+    }
