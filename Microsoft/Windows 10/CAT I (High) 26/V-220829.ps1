7692a6f73df03dd7132bba6fbf57565afa0addb1
svetlyobg <swk_12@abv.bg>
2021-05-21 21:08:55 +0300

    V-220829	- Autoplay must be disabled for all drives

---
+++ Microsoft/Windows 10/CAT I (High) 26/V-220829.ps1
@@ -0,0 +1,22 @@
+﻿# V-220829	- Autoplay must be disabled for all drives
+# https://www.stigviewer.com/stig/windows_10/2020-10-15/finding/V-220829
+
+$NoDriveTypeAutoRun = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer\' | Select -ExpandProperty NoDriveTypeAutoRun
+
+    if ( $NoDriveTypeAutoRun -eq 255 )
+        {
+            Write-Host "`n$line"
+            Write-Host V-220829 result is $NoDriveTypeAutoRun 
+            Write-Host "`n$line"
+            Write-Host "This is not a finding" -ForegroundColor Green
+            Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer\'
+            }
+
+    else
+            {
+                Write-Host "`n$line"
+                Write-Host V-220829 result is $NoDriveTypeAutoRun
+                Write-Host "`n$line"
+                Write-Host "This is a finding" -ForegroundColor Red
+                Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer\'
+            }
