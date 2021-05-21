06cc5ac82e943a316a7c3f04c5a7d05fe097c240
svetlyobg <swk_12@abv.bg>
2021-05-21 21:14:56 +0300

    V-220831 - Microsoft consumer experiences must be turned off

---
+++ Microsoft/Windows 10/CAT III (Low) 18/V-220831.ps1
@@ -0,0 +1,22 @@
+﻿# V-220831 - Microsoft consumer experiences must be turned off
+# https://www.stigviewer.com/stig/windows_10/2020-10-15/finding/V-220831
+
+$DisableWindowsConsumerFeatures = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CloudContent\' | Select -ExpandProperty DisableWindowsConsumerFeatures
+
+    if ( $NoDriveTypeAutoRun -eq 1 )
+        {
+            Write-Host "`n$line"
+            Write-Host V-220831 result is $DisableWindowsConsumerFeatures 
+            Write-Host "`n$line"
+            Write-Host "This is not a finding" -ForegroundColor Green
+            Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CloudContent\'
+            }
+
+    else
+            {
+                Write-Host "`n$line"
+                Write-Host V-220831 result is $DisableWindowsConsumerFeatures
+                Write-Host "`n$line"
+                Write-Host "This is a finding" -ForegroundColor Red
+                Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CloudContent\'
+            }
