b022f1e15864b4a1376a32c42546f4829a27103e
svetlyobg <swk_12@abv.bg>
2021-05-21 17:08:15 +0300

    V-220958 - The Act as part of the operating system user right must not be assigned to any groups or accounts

---
+++ Microsoft/Windows 10/CAT I (High) 26/no-V-220958.ps1
@@ -0,0 +1,3 @@
+﻿# V-220958 - The Act as part of the operating system user right must not be assigned to any groups or accounts
+# https://stigviewer.com/stig/windows_10/2020-10-15/finding/V-220958
+
