218282b23462c82b74855756d8253898dfab6dcb
svetlyobg <swk_12@abv.bg>
2021-05-21 21:01:35 +0300

    V-220929 - Anonymous enumeration of SAM accounts must not be allowed

---
+++ Microsoft/Windows 10/CAT I (High) 26/V-220929.ps1
@@ -0,0 +1,23 @@
+﻿# V-220929 - Anonymous enumeration of SAM accounts must not be allowed
+# https://www.stigviewer.com/stig/windows_10/2020-10-15/finding/V-220929
+
+$RestrictAnonymousSAM = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\' | Select -ExpandProperty restrictanonymoussam
+
+    if ($RestrictAnonymousSAM -eq 1)
+        {
+            Write-Host "`n$line"
+            Write-Host V-220929 result is $RestrictAnonymousSAM 
+            Write-Host "`n$line"
+            Write-Host "This is not a finding" -ForegroundColor Green
+            Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\'
+    
+        }
+
+    else
+        {
+            Write-Host "`n$line"
+            Write-Host V-220929 result is $RestrictAnonymousSAM
+            Write-Host "`n$line"
+            Write-Host "This is a finding" -ForegroundColor Red
+            Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\'
+        }
