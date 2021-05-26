8753c5a07a3f8f3c71805e8da37c4531a98e68d3
svetlyobg <swk_12@abv.bg>
2021-05-26 11:31:52 +0300

    V-220700 - Secure Boot must be enabled on Windows 10 systems

---
+++ Microsoft/Windows 10/CAT III (Low) 18/V-220700.ps1
@@ -0,0 +1,22 @@
+﻿# V-220700 - Secure Boot must be enabled on Windows 10 systems
+# https://stigviewer.com/stig/windows_10/2020-10-15/finding/V-220700
+
+$ConfirmSecureBootUEFI = Confirm-SecureBootUEFI
+
+    if ( $ConfirmSecureBootUEFI -eq 'True' )
+        {
+            Write-Host "`n$line"
+            Write-Host V-220700 result is $ConfirmSecureBootUEFI 
+            Write-Host "`n$line"
+            Write-Host "This is not a finding" -ForegroundColor Green
+            Get-ItemProperty -Path 'Confirm-SecureBootUEF'
+        }
+
+    else
+        {
+            Write-Host "`n$line"
+            Write-Host V-220700 result is $ConfirmSecureBootUEFI
+            Write-Host "`n$line"
+            Write-Host "This is a finding" -ForegroundColor Red
+            Confirm-SecureBootUEFI
+        }
