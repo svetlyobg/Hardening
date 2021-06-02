3b1788cef5411b4f8d67374ce6d981ef9e0f83e7
svetlyobg <swk_12@abv.bg>
2021-06-02 16:31:27 +0300

    V-220825 - The setting to allow Microsoft accounts to be optional for modern style apps must be enabled

---
+++ Microsoft/Windows 10/CAT III (Low) 18/V-220825.ps1
@@ -0,0 +1,23 @@
+﻿# V-220825 - The setting to allow Microsoft accounts to be optional for modern style apps must be enabled
+# https://stigviewer.com/stig/windows_10/2021-03-10/finding/V-220825
+
+$MSAOptional = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\' | Select -ExpandProperty MSAOptional
+
+ if ( $MSAOptional -eq 1 )
+        {
+            Write-Host "`n$line"
+            Write-Host V-220825 result is $MSAOptional 
+            Write-Host "`n$line"
+            Write-Host "This is not a finding" -ForegroundColor Green
+            Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\'
+            }
+
+
+    else
+            {
+                Write-Host "`n$line"
+                Write-Host V-220825 result is $MSAOptional
+                Write-Host "`n$line"
+                Write-Host "This is a finding" -ForegroundColor Red
+                Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\'
+            } 
