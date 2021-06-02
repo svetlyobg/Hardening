80b1c1cb88a0f8b16f63d01b6fd47be7c62a7e61
svetlyobg <swk_12@abv.bg>
2021-06-02 16:37:23 +0300

    V-220711 - Unused accounts must be disabled or removed from the system after 35 days of inactivity

---
+++ Microsoft/Windows 10/CAT III (Low) 18/V-220711.ps1
@@ -0,0 +1,12 @@
+﻿# V-220711 - Unused accounts must be disabled or removed from the system after 35 days of inactivity
+# https://stigviewer.com/stig/windows_10/2021-03-10/finding/V-220711
+
+([ADSI]('WinNT://{0}' -f $env:COMPUTERNAME)).Children | Where { $_.SchemaClassName -eq 'user' } | ForEach {
+$user = ([ADSI]$_.Path)
+$lastLogin = $user.Properties.LastLogin.Value
+$enabled = ($user.Properties.UserFlags.Value -band 0x2) -ne 0x2
+if ($lastLogin -eq $null) {
+$lastLogin = 'Never'
+}
+Write-Host $user.Name $lastLogin $enabled
+}
