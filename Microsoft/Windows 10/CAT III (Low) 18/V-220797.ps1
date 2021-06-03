770929455dbc505fca060a36dc0e0627a019a380
svetlyobg <swk_12@abv.bg>
2021-06-03 17:05:20 +0300

    V-220797 - The system must be configured to prevent Internet Control Message Protocol (ICMP) redirects from overriding Open Shortest Path First (OSPF) generated routes

---
+++ Microsoft/Windows 10/CAT III (Low) 18/V-220797.ps1
@@ -0,0 +1,22 @@
+﻿# V-220797 - The system must be configured to prevent Internet Control Message Protocol (ICMP) redirects from overriding Open Shortest Path First (OSPF) generated routes
+# https://stigviewer.com/stig/windows_10/2021-03-10/finding/V-220797
+
+$EnableICMPRedirect = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\' | Select -ExpandProperty EnableICMPRedirect
+
+ if ( $EnableICMPRedirect -eq 0 )
+        {
+            Write-Host "`n$line"
+            Write-Host V-220797 result is $EnableICMPRedirect 
+            Write-Host "`n$line"
+            Write-Host "This is not a finding" -ForegroundColor Green
+            Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters\'
+            }
+
+    else
+            {
+                Write-Host "`n$line"
+                Write-Host V-220797 result is $EnableICMPRedirect
+                Write-Host "`n$line"
+                Write-Host "This is a finding" -ForegroundColor Red
+                Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters\'
+            } 
