c082ad4f2d6dbfb5a7e82ef5d0252d4523e0a179
svetlyobg <swk_12@abv.bg>
2021-06-03 17:13:49 +0300

    V-220826 - The Application Compatibility Program Inventory must be prevented from collecting data and sending the information to Microsoft

---
+++ Microsoft/Windows 10/CAT III (Low) 18/V-220826.ps1
@@ -0,0 +1,22 @@
+﻿# V-220826 - The Application Compatibility Program Inventory must be prevented from collecting data and sending the information to Microsoft
+# https://stigviewer.com/stig/windows_10/2021-03-10/finding/V-220826
+
+$DisableInventory = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat\' | Select -ExpandProperty  DisableInventory
+
+ if ( $DisableInventory -eq 1 )
+        {
+            Write-Host "`n$line"
+            Write-Host V-220826 result is $DisableInventory 
+            Write-Host "`n$line"
+            Write-Host "This is not a finding" -ForegroundColor Green
+            Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat\'
+            }
+
+    else
+            {
+                Write-Host "`n$line"
+                Write-Host V-220826 result is $DisableInventory
+                Write-Host "`n$line"
+                Write-Host "This is a finding" -ForegroundColor Red
+                Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat\'
+            } 
