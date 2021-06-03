157ede046ee5289ae239d6a8b3aa5a5d57bd1a06
svetlyobg <swk_12@abv.bg>
2021-06-03 17:09:14 +0300

    V-220798 - The system must be configured to ignore NetBIOS name release requests except from WINS servers

---
+++ Microsoft/Windows 10/CAT III (Low) 18/V-220798.ps1
@@ -0,0 +1,23 @@
+﻿# V-220798 - The system must be configured to ignore NetBIOS name release requests except from WINS servers
+# https://stigviewer.com/stig/windows_10/2021-03-10/finding/V-220798
+# Legacy
+
+$NoNameReleaseOnDemand = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Netbt\Parameters\' | Select -ExpandProperty  NoNameReleaseOnDemand
+
+ if ( $NoNameReleaseOnDemand -eq 1 )
+        {
+            Write-Host "`n$line"
+            Write-Host V-220798 result is $NoNameReleaseOnDemand 
+            Write-Host "`n$line"
+            Write-Host "This is not a finding" -ForegroundColor Green
+            Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Netbt\Parameters\'
+            }
+
+    else
+            {
+                Write-Host "`n$line"
+                Write-Host V-220798 result is $NoNameReleaseOnDemand
+                Write-Host "`n$line"
+                Write-Host "This is a finding" -ForegroundColor Red
+                Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Netbt\Parameters\'
+            } 
