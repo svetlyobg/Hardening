f6307def0337c239e5e4427fffa255a4ef436b02
svetlyobg <swk_12@abv.bg>
2021-06-29 16:24:53 +0300

    V-70969 - Caching of logon credentials must be limited

---
+++ Microsoft/MS Office/Excel 2016/CAT II (Medium) 41/V-70969.ps1
@@ -0,0 +1,63 @@
+﻿# V-70969 - Caching of logon credentials must be limited
+# https://stigviewer.com/stig/microsoft_excel_2016/2017-09-19/finding/V-70969
+
+
+$excel = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_SAFE_BINDTOOBJECT" | Select -ExpandProperty 'excel.exe'
+
+function fexcel
+{
+
+                    $excel = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_SAFE_BINDTOOBJECT" | Select -ExpandProperty 'excel.exe'
+
+                    if ( $excel -eq 1 )
+                        {
+                            Write-Host "`n$line"
+                            Write-Host V-70969 result is $excel 
+                            Write-Host "`n$line"
+                            Write-Host "This is not a finding" -ForegroundColor Green
+                            Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_SAFE_BINDTOOBJECT'
+
+                            Write-Host "The Window will close in 5 seconds"
+                            Start-Sleep -Seconds 5
+                            }
+
+                    else
+                            {
+                                Write-Host "`n$line"
+                                Write-Host V-70969 result is $excel
+                                Write-Host "`n$line"
+                                Write-Host "This is a finding" -ForegroundColor Red
+                                Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_SAFE_BINDTOOBJECT'
+
+                                Write-Host "The Window will close in 5 seconds"
+                                Start-Sleep -Seconds 5
+                            } 
+
+                }
+
+
+if ("Test-Path 'HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_SAFE_BINDTOOBJECT'" -like 'True')
+
+{
+
+
+if ( $excel  -like "True" )
+        {
+           fexcel
+
+           }
+}
+else 
+
+    {
+        Write-Host NoPathFound -ForegroundColor Red
+        New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\' -Name PushNotifications
+        Write-Host Path Created -ForegroundColor Green
+
+        New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_SAFE_BINDTOOBJECT' -Name 'excel.exe' -Value 1 -PropertyType DWORD -Force
+
+        fexcel
+
+
+
+    } 
