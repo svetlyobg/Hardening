a314a76be0bd14cf1aca752db886b36c7fbf9e91
svetlyobg <swk_12@abv.bg>
2021-06-29 17:39:52 +0300

    V-71003 - File Downloads must be configured for proper restrictions

---
+++ Microsoft/MS Office/Excel 2016/CAT II (Medium) 41/V-71003.ps1
@@ -0,0 +1,61 @@
+﻿# V-71003 - File Downloads must be configured for proper restrictions
+# https://stigviewer.com/stig/microsoft_excel_2016/2017-09-19/finding/V-71003
+
+
+$excel = Get-ItemProperty -Path "HKLM:\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_FILEDOWNLOAD" | Select -ExpandProperty 'excel.exe'
+
+function fexcel
+{
+
+                    $excel = Get-ItemProperty -Path "HKLM:\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_FILEDOWNLOAD" | Select -ExpandProperty 'excel.exe'
+
+                    if ( $excel -eq 1 )
+                        {
+                            Write-Host "`n$line"
+                            Write-Host V-70997 result is $excel 
+                            Write-Host "`n$line"
+                            Write-Host "This is not a finding" -ForegroundColor Green
+                            Get-ItemProperty -Path 'HKLM:\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_FILEDOWNLOAD'
+
+                            Write-Host "The Window will close in 5 seconds"
+                            Start-Sleep -Seconds 5
+                            }
+
+                    else
+                            {
+                                Write-Host "`n$line"
+                                Write-Host V-70997 result is $excel
+                                Write-Host "`n$line"
+                                Write-Host "This is a finding" -ForegroundColor Red
+                                Get-ItemProperty -Path 'HKLM:\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_FILEDOWNLOAD'
+
+                                Write-Host "The Window will close in 5 seconds"
+                                Start-Sleep -Seconds 5
+                            } 
+
+                }
+
+
+if ("Test-Path 'HKLM:\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_FILEDOWNLOAD'" -like 'True')
+
+{
+
+
+if ( $excel  -like "True" )
+        {
+           fexcel
+
+           }
+}
+else 
+
+    {
+       
+
+        New-ItemProperty -Path 'HKLM:\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_FILEDOWNLOAD' -Name 'excel.exe' -Value 1 -PropertyType DWORD -Force
+
+        fexcel
+
+
+
+    } 
