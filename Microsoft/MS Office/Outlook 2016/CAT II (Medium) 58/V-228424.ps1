531686f86d1f36fb4f954c0e8c621f42cfaca128
svetlyobg <swk_12@abv.bg>
2021-07-02 17:58:28 +0300

    V-228424 - Add-on Management functionality must be allowed

---
+++ Microsoft/MS Office/Outlook 2016/CAT II (Medium) 58/V-228424.ps1
@@ -0,0 +1,59 @@
+﻿<#
+.SYNOPSIS
+     V-228424 - Add-on Management functionality must be allowed
+
+.DESCRIPTION
+    Internet Explorer add-ons are pieces of code, run in Internet Explorer, to provide additional functionality. Rogue add-ons may contain viruses or other malicious code. Disabling or not configuring this setting could allow malicious code or users to become active on user computers or the network. For example, a malicious user can monitor and then use keystrokes users type into Internet Explorer. Even legitimate add-ons may demand resources, compromising the performance of Internet Explorer, and the operating systems for user computers
+
+.LINK
+    https://stigviewer.com/stig/microsoft_outlook_2016/2020-09-25/finding/V-228424
+#>
+
+$path = 'HKLM:\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_ADDON_MANAGEMENT\'
+$regname = 'outlook.exe'
+$regvalue = Get-ItemProperty -Path $path | Select -ExpandProperty $regname
+
+
+function fcheckpath
+{Test-Path $path}
+
+function fcreate
+{
+        Write-Host NoPathFound -ForegroundColor Red
+        New-Item -Path $path -ItemType Directory
+        Write-Host Path Created -ForegroundColor Green
+        New-ItemProperty -Path $path -Name $regname -Value 1 -PropertyType DWORD -Force
+        Write-Host Registry Created and Set Up -ForegroundColor Green
+
+}
+
+function fcheckreg 
+    {
+    $regvalue = Get-ItemProperty -Path $path | Select -ExpandProperty $regname
+        if ( $regvalue -eq 1 )
+        {
+            Write-Host "`n$line"
+            Write-Host  V-228424 result is $regvalue 
+            Write-Host "`n$line"
+            Write-Host "This is not a finding" -ForegroundColor Green
+            Get-ItemProperty -Path $path
+            Write-Host "The Window will close in 5 seconds"
+            Start-Sleep -Seconds 5
+            }
+
+    else
+            {
+                Write-Host "`n$line"
+                Write-Host  V-228424 result is $regvalue
+                Write-Host "`n$line"
+                Write-Host "This is a finding" -ForegroundColor Red
+                Get-ItemProperty -Path $path
+                Write-Host "The Window will close in 5 seconds"
+                Start-Sleep -Seconds 5
+            } 
+    }
+    
+if (fcheckpath -like 'True' )
+{fcreate
+ fcheckreg}
+else{fcheckreg}
