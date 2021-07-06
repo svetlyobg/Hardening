ed6d35a5b8cc846914f82f9e03de97f0e803df1c
svetlyobg <swk_12@abv.bg>
2021-07-06 17:12:36 +0300

    V-228444 - Custom Outlook Object Model (OOM) action execution prompts must be configured

---
+++ Microsoft/MS Office/Outlook 2016/CAT II (Medium) 58/V-228444.ps1
@@ -0,0 +1,74 @@
+﻿<#
+.SYNOPSIS
+     V-228444 - Custom Outlook Object Model (OOM) action execution prompts must be configured
+
+.DESCRIPTION
+    This policy setting controls whether Outlook prompts users before executing a custom action. Custom actions add functionality to Outlook that can be triggered as part of a rule. Among other possible features, custom actions can be created that reply to messages in ways that circumvent the Outlook model's programmatic send protections. If you enable this policy setting, you can choose from four options to control how Outlook functions when a custom action is executed that uses the Outlook object model: * Prompt User * Automatically Approve * Automatically Deny * Prompt user based on computer security. This option enforces the default configuration in Outlook. If you disable or do not configure this policy setting, when Outlook or another program initiates a custom action using the Outlook object model, users are prompted to allow or reject the action. If this configuration is changed, malicious code can use the Outlook object model to compromise sensitive information or otherwise cause data and computing resources to be at risk. This is the equivalent of choosing Enabled -- Prompt user based on computer security. 
+
+.LINK
+    https://stigviewer.com/stig/microsoft_outlook_2016/2020-09-25/finding/V-228444
+#>
+
+$path = 'HKCU:\Software\Policies\Microsoft\Office\16.0\outlook\security'
+$regname = 'PromptOOMCustomAction'
+$regvalue = Get-ItemProperty -Path $path | Select -ExpandProperty $regname
+
+
+function fcheckpath
+{Test-Path $path}
+
+function fcreate
+{
+        Write-Host NoPathFound -ForegroundColor Red
+        New-Item -Path $path -ItemType Directory
+        Write-Host Path Created -ForegroundColor Green
+        New-ItemProperty -Path $path -Name $regname -Value 0 -PropertyType DWORD -Force
+        Write-Host Registry Created and Set Up -ForegroundColor Green
+
+}
+
+function fcheckreg 
+    {
+    $regvalue = Get-ItemProperty -Path $path | Select -ExpandProperty $regname
+        if ( $regvalue -eq 0 )
+        {
+            Write-Host "`n$line"
+            Write-Host  V-228444 result is $regvalue 
+            Write-Host "`n$line"
+            Write-Host "This is not a finding" -ForegroundColor Green
+            Get-ItemProperty -Path $path
+            Write-Host "The Window will close in 5 seconds"
+            Start-Sleep -Seconds 5
+            }
+
+    else
+            {
+                Write-Host "`n$line"
+                Write-Host  V-228444 result is $regvalue
+                Write-Host "`n$line"
+                Write-Host "This is a finding" -ForegroundColor Red
+                Get-ItemProperty -Path $path
+                Write-Host "The Window will close in 5 seconds"
+                Start-Sleep -Seconds 5
+            } 
+    }
+
+
+if ("Test-Path $path" -like "True")
+{
+    Write-Host $path
+    fcreate
+    fcheckreg
+}
+
+else {
+        Write-Host $path
+        fcreate
+     }
+
+
+    
+if (fcheckpath -like 'True' )
+{fcreate
+ fcheckreg}
+else{fcheckreg}
