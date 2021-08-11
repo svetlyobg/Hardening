2e2efef933474b954ae49e2a4afdd948a8c1e2d1
svetlyobg <swk_12@abv.bg>
2021-08-11 13:24:26 +0300

    V-228472 - Automatically downloading enclosures on RSS must be disallowed
    This policy setting allows you to control whether Outlook automatically downloads enclosures on RSS items. If you enable this policy setting, Outlook will automatically download enclosures on RSS items. If you disable or do not configure this policy setting, enclosures on RSS items are not downloaded by default.

--- Microsoft/MS Office/Outlook 2016/CAT II (Medium) 58/V-228472.bat
+++ Microsoft/MS Office/Outlook 2016/CAT II (Medium) 58/V-228472.bat
@@ -2,8 +2,8 @@ rem V-228472 - Automatically downloading enclosures on RSS must be disallowed
 rem https://www.stigviewer.com/stig/microsoft_outlook_2016/2020-09-25/finding/V-228472
 rem This policy setting allows you to control whether Outlook automatically downloads enclosures on RSS items. If you enable this policy setting, Outlook will automatically download enclosures on RSS items. If you disable or do not configure this policy setting, enclosures on RSS items are not downloaded by default. 
 
-reg add HKCU\Software\Policies\Microsoft\Office\12.0\outlook\options\rss /v EnableAttachments /t REG_DWORD /d 0 /f
-reg add HKCU\Software\Policies\Microsoft\Office\13.0\outlook\options\rss /v EnableAttachments /t REG_DWORD /d 0 /f
-reg add HKCU\Software\Policies\Microsoft\Office\14.0\outlook\options\rss /v EnableAttachments /t REG_DWORD /d 0 /f
+rem reg add HKCU\Software\Policies\Microsoft\Office\12.0\outlook\options\rss /v EnableAttachments /t REG_DWORD /d 0 /f
+rem reg add HKCU\Software\Policies\Microsoft\Office\13.0\outlook\options\rss /v EnableAttachments /t REG_DWORD /d 0 /f
+rem reg add HKCU\Software\Policies\Microsoft\Office\14.0\outlook\options\rss /v EnableAttachments /t REG_DWORD /d 0 /f
 reg add HKCU\Software\Policies\Microsoft\Office\15.0\outlook\options\rss /v EnableAttachments /t REG_DWORD /d 0 /f
-reg add HKCU\Software\Policies\Microsoft\Office\16.0\outlook\options\rss /v EnableAttachments /t REG_DWORD /d 0 /f
+rem reg add HKCU\Software\Policies\Microsoft\Office\16.0\outlook\options\rss /v EnableAttachments /t REG_DWORD /d 0 /f
