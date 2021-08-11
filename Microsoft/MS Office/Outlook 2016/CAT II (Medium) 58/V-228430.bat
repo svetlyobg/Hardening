be8d830ca936b951baf0d488297f35578069b0e9
svetlyobg <swk_12@abv.bg>
2021-08-11 17:41:12 +0300

    V-228430 - Publishing to a Web Distributed and Authoring (DAV) server must be prevented
     This policy setting controls whether Outlook users can publish their calendars to a DAV server. If you enable this policy setting, Outlook users cannot publish their calendars to a DAV server. If you disable or do not configure this policy setting, Outlook users can share their calendars with others by publishing them to a server that supports the World Wide Web Distributed Authoring and Versioning (WebDAV) protocol.

--- Microsoft/MS Office/Outlook 2016/CAT II (Medium) 58/V-228430.bat
+++ Microsoft/MS Office/Outlook 2016/CAT II (Medium) 58/V-228430.bat
@@ -1,3 +1,4 @@
 rem V-228430 - Publishing to a Web Distributed and Authoring (DAV) server must be prevented
 rem https://www.stigviewer.com/stig/microsoft_outlook_2016/2020-09-25/finding/V-228430
 
+reg add HKCU\Software\Policies\Microsoft\Office\16.0\outlook\options\pubcal /v DisableDav /t REG_DWORD /d 1 /f
