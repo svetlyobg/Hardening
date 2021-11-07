cbdffe34858513b1d91e1340882b1b9a32e214c1
svetlyobg <swk_12@abv.bg>
2021-11-07 14:40:42 +0200

    create Windows 10 STIGs - category 2 meduim 241

---
+++ Microsoft/Windows 10/CAT II (Med) 241/2-cat-241-windows-10.bat
@@ -0,0 +1,13 @@
+rem V-220709 - Alternate operating systems must not be permitted on the same system
+rem https://www.stigviewer.com/stig/windows_10/2021-03-10/finding/V-220709
+rem manual check
+rem Run "Advanced System Settings".
+rem Select the "Advanced" tab.
+rem Click the "Settings" button in the "Startup and Recovery" section.
+rem If the drop-down list box "Default operating system:" shows any operating system other than Windows 10, this is a finding.
+
+rem V-220830 - Enhanced anti-spoofing for facial recognition must be enabled on Window 10.
+rem https://www.stigviewer.com/stig/windows_10/2021-03-10/finding/V-220830
+reg add "HKLM\Software\Policies\Microsoft\Biometrics\FacialFeatures" /v EnhancedAntiSpoofing /t REG_DWORD /d 1 /f
+reg add EnhancedAntiSpoofing REG_DWORD 1
+echo enabled "V-220830 - Enhanced anti-spoofing for facial recognition must be enabled on Window 10"
