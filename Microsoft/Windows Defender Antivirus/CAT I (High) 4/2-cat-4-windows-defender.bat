93741d9d1a5765496de7b994c77f13106db4c80d
svetlyobg <swk_12@abv.bg>
2021-12-06 17:33:46 +0200

    Finish with batch file for implementing HIGH MS Windows Defender Antivirus STIGs

--- Microsoft/Windows Defender Antivirus/CAT I (High) 4/2-cat-4-windows-defender.bat
+++ Microsoft/Windows Defender Antivirus/CAT I (High) 4/2-cat-4-windows-defender.bat
@@ -1,3 +1,17 @@
 rem https://stigviewer.com/stig/ms_windows_defender_antivirus/2020-05-12/finding/V-75147
 rem V-75147 - Windows Defender AV must be configured to block the Potentially Unwanted Application (PUA) feature
 reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v PUAProtection /t REG_DWORD /d 1 /f
+
+rem https://stigviewer.com/stig/ms_windows_defender_antivirus/2020-05-12/finding/V-75153
+rem V-75153 - Windows Defender AV must be configured to run and scan for malware and other potentially unwanted software
+rem manual check - should be disabled if there is another AV installed
+rem PowerShell -> Uninstall-WindowsFeature -Name Windows-Defender
+rem reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+
+rem https://stigviewer.com/stig/ms_windows_defender_antivirus/2020-05-12/finding/V-75241
+rem V-75241 - Windows Defender AV spyware definition age must not exceed 7 dayss
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Signature Updates" /v ASSignatureDue /t REG_DWORD /d 7 /f
+
+rem https://stigviewer.com/stig/ms_windows_defender_antivirus/2020-05-12/finding/V-75241
+rem V-75241 - Windows Defender AV spyware definition age must not exceed 7 dayss
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Signature Updates" /v AVSignatureDue /t REG_DWORD /d 7 /f
