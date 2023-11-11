7eb3000f9348de8c8ca0fbc3d5ae6d4e40ed3697
svetlyobg <swk_12@abv.bg>
2023-11-11 16:15:46 +0200

    Ensure 'Downloaded Maps Manager (MapsBroker)' is set to 'Disabled'

--- Microsoft/Windows 10/CIS Benchmark for Windows 10 Enterprise (Release 1803)/CIS Benchmark for Windows 10 Enterprise (Release 1803).cmd
+++ Microsoft/Windows 10/CIS Benchmark for Windows 10 Enterprise (Release 1803)/CIS Benchmark for Windows 10 Enterprise (Release 1803).cmd
@@ -36,3 +36,6 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogo
 
 rem Ensure 'Bluetooth Support Service (bthserv)' is set to 'Disabled'
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\bthserv"/v Start /t REG_DWORD /d 4 /f
+
+rem Ensure 'Downloaded Maps Manager (MapsBroker)' is set to 'Disabled'
+g add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\MapsBroker"/v Start /t REG_DWORD /d 4 /f
