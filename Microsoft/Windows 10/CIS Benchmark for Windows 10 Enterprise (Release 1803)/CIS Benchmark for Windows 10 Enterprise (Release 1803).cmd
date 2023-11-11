46e81d38f038b036e5d3bccaff24bf9adf7f2d27
svetlyobg <swk_12@abv.bg>
2023-11-11 15:12:16 +0200

    Ensure 'Interactive logon: Number of previous logons to cache (in case domain controller is not available)' is set to '4 or fewer logon(s)'

--- Microsoft/Windows 10/CIS Benchmark for Windows 10 Enterprise (Release 1803)/CIS Benchmark for Windows 10 Enterprise (Release 1803).cmd
+++ Microsoft/Windows 10/CIS Benchmark for Windows 10 Enterprise (Release 1803)/CIS Benchmark for Windows 10 Enterprise (Release 1803).cmd
@@ -32,4 +32,4 @@ rem Service supporting the audio gateway role of the Bluetooth Handsfree Profile
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\BTAGService" /v Start /t REG_DWORD /d 4 /f
 
 rem Ensure 'Interactive logon: Number of previous logons to cache (in case domain controller is not available)' is set to '4 or fewer logon(s)'
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v CachedLogonsCount /t REG_DWORD /d n:^(\d+) compare <= 4 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v CachedLogonsCount /t REG_DWORD /d 4 /f
