cd3c0cb21fa561f54c6622aff2f6c0543972216f
svetlyobg <swk_12@abv.bg>
2023-11-11 16:19:54 +0200

    Ensure 'LxssManager (LxssManager)' is set to 'Disabled' or 'Not Installed'

--- Microsoft/Windows 10/CIS Benchmark for Windows 10 Enterprise (Release 1803)/CIS Benchmark for Windows 10 Enterprise (Release 1803).cmd
+++ Microsoft/Windows 10/CIS Benchmark for Windows 10 Enterprise (Release 1803)/CIS Benchmark for Windows 10 Enterprise (Release 1803).cmd
@@ -48,3 +48,6 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess"/v St
 
 rem Ensure 'Link-Layer Topology Discovery Mapper (lltdsvc)' is set to 'Disabled'
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\lltdsvc"/v Start /t REG_DWORD /d 4 /f
+
+rem Ensure 'LxssManager (LxssManager)' is set to 'Disabled' or 'Not Installed'
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LxssManager"/v Start /t REG_DWORD /d 4 /f
